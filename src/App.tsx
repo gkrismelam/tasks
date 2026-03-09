302caf1de6377e147860919e6b1e58a80ecde852
Gopi Melam <gmelam@udel.edu>
2026-03-08 23:45:09 -0400

    Resolve merge conflict by incorporating both suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { ChangeType } from "./components/ChangeType";
+// import { RevealAnswer } from "./components/RevealAnswer";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): React.JSX.Element {
     return (
@@ -17,42 +13,10 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <p>Gopi Melam</p>
-            <p>Hello World</p>
-            <div>
-                <h1>Header</h1>
-                <ul>
-                    <li>CISC 275</li>
-                    <li>MATH 450</li>
-                    <li>STAT 200</li>
-                </ul>
-                <Button
-                    onClick={() => {
-                        console.log("Hello World!");
-                    }}
-                >
-                    Log Hello World
-                </Button>
-                <img alt="place hodler text" />
-            </div>
-            <div style={{ backgroundColor: "red" }} className="App-div">
-                <Container>
-                    <Row>
-                        <Col>Column 2</Col>
-                        <Col>Column 1</Col>
-                    </Row>
-                </Container>
-            </div>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
-            <RevealAnswer></RevealAnswer>
+            {/* <RevealAnswer></RevealAnswer>
             <hr />
             <StartAttempt></StartAttempt>
             <hr />
@@ -60,8 +24,7 @@ function App(): React.JSX.Element {
             <hr />
             <ChangeType></ChangeType>
             <hr />
-            <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
+            <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
