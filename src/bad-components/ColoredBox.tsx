a4f31680cce31521211ac475a182ff667f606b5b
Gopi Melam <gmelam@udel.edu>
2026-03-12 02:18:33 -0400

    Fixed all bad components with tests passing

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+//import { Color } from "react-bootstrap/esm/types";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+interface ColoredBoxProps {
+    setColorIndex: (newValue: number) => void;
+    colorIndex: number;
+}
+
+interface ColorPreviewProp {
+    colorIndex: number;
+}
+
+function ChangeColor({
+    setColorIndex,
+    colorIndex,
+}: ColoredBoxProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -17,14 +29,14 @@ function ChangeColor(): React.JSX.Element {
     );
 }
 
-function ColorPreview(): React.JSX.Element {
+function ColorPreview({ colorIndex }: ColorPreviewProp): React.JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px",
@@ -34,13 +46,17 @@ function ColorPreview(): React.JSX.Element {
 }
 
 export function ColoredBox(): React.JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    colorIndex={colorIndex}
+                    setColorIndex={setColorIndex}
+                ></ChangeColor>
+                <ColorPreview colorIndex={colorIndex}></ColorPreview>
             </div>
         </div>
     );
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface ShoveBoxProps {
+    setPosition: (newValue: number) => void;
+    position: number;
+}
+
+interface MoveableBoxProps {
+    position: number;
+}
+
 function ShoveBoxButton({
     position,
     setPosition,
-}: {
-    position: number;
-    setPosition: (newPosition: number) => void;
-}) {
+}: ShoveBoxProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -19,8 +25,7 @@ function ShoveBoxButton({
     );
 }
 
-function MoveableBox(): React.JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position }: MoveableBoxProps): React.JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -38,19 +43,19 @@ function MoveableBox(): React.JSX.Element {
 }
 
 export function ShoveBox(): React.JSX.Element {
-    const box = MoveableBox();
-
+    const [position, setPosition] = useState<number>(10);
+    const box = <MoveableBox position={position}></MoveableBox>;
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
